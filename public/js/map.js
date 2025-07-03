f19ca7a6f39c3354e7769f45518995e9118035d3
Ashfaq Kadir Ovinno <akovinno.official@gmail.com>
2025-07-03 11:47:15 +0600

    Mapping codes added using leaflet

--- public/js/map.js
+++ public/js/map.js
@@ -1,22 +1,11 @@
-document.addEventListener("DOMContentLoaded", function () {
-  if (window.listingMapData && document.getElementById("my-map")) {
-    const { latitude, longitude, apiKey, title } = window.listingMapData;
-
-    const map = L.map("my-map").setView([latitude, longitude], 10);
-    const isRetina = L.Browser.retina;
-    const baseUrl =
-      "https://maps.geoapify.com/v1/tile/osm-bright/{z}/{x}/{y}.png?apiKey=" + apiKey;
-    const retinaUrl =
-      "https://maps.geoapify.com/v1/tile/osm-bright/{z}/{x}/{y}@2x.png?apiKey=" + apiKey;
-    L.tileLayer(isRetina ? retinaUrl : baseUrl, {
-      maxZoom: 20,
-      id: "osm-bright",
-    }).addTo(map);
-
-    const tooltipContent = `${title}<br><span style="font-size:0.95rem;font-weight:normal;">Exact location will be provided after booking</span>`;
-
-    L.marker([latitude, longitude])
-      .addTo(map)
-      .bindTooltip(tooltipContent, { permanent: false, direction: "top" });
-  }
-});
+const coords = JSON.parse(coordinates);
+const lat = coords[1];
+const lng = coords[0];
+const map = L.map('my-map').setView([lat, lng], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    
+}).addTo(map);
+const marker = L.marker([lat, lng])
+  .addTo(map)
+  .bindPopup("<b>" + title + "</b><br>Exact location will be provided after booking").openPopup();
